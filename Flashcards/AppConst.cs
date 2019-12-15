d821732952244a65564d91709c5fb20c476ae3f8
Hary Prabowo <h.prabowo@allegrodevelopment.com>
2019-12-15 22:03:41 +0700

    Add constant and vocabularies for each deck

---
+++ Flashcards/AppConst.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flashcards
+{
+    public class AppConst
+    {
+        public const int DECKNUMS = 18;
+    }
+}
--- Flashcards/Vocabularies.cs
+++ Flashcards/Vocabularies.cs
@@ -14,14 +14,34 @@ namespace Flashcards
             {                
                 sr.ReadLine();  //Skip the header
 
-                Dictionary<string, string> pairs = new Dictionary<string, string>();
+                List<Tuple<string, string>> vocabs = new List<Tuple<string, string>>();
+                Dictionary<int, List<Tuple<string, string>>> vocabDictionary = new Dictionary<int, List<Tuple<string, string>>>();
+
+                int counter = 0;
                 while (!sr.EndOfStream)
                 {
                     var line = sr.ReadLine();
                     var values = line.Split(',');
 
-                    pairs.Add(values[0].Trim(), values[1].Trim());
+                    vocabs.Add(new Tuple<string, string>(values[0].Trim(), values[1].Trim()));
+
+                    counter++;
                 }
+
+                int cards = vocabs.Count / AppConst.DECKNUMS + 1;
+                for (int i = 0; i < AppConst.DECKNUMS; i++)
+                {
+                    List<Tuple<string, string>> segment = new List<Tuple<string, string>>();
+
+                    int startIndex = i * cards;
+                    for (int j = startIndex; j < vocabs.Count && j < startIndex + cards; j++)
+                    {
+                        segment.Add(vocabs[j]);
+                    }
+
+                    vocabDictionary.Add(i + 1, segment);
+                }
+
             }
         }
     }
